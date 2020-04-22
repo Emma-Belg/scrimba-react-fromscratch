acd033a8a7f4006a9b9bda4191e14d62ea29bd5b
emma <emma.wishart@live.com.au>
2020-04-22 15:31:22 +0200

    fixed my problem by switching to the jsx reader file thing in phpstorm

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component} from "react";
 import "./App.css";
 
-class App extends Component{
-    render(){
+function App(){
+
         return(
             <div className="App">
             <h1> Hello, World! </h1>
-        </div>
-    );
-    }
+            </div>
+        );
 }
 
 export default App;
