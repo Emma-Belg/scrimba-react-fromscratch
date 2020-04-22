8137ac1f919bb1e327fd07fb60e136c1ce9aac65
emma <emma.wishart@live.com.au>
2020-04-22 17:49:33 +0200

    the app isn't much but I understand how react works and what it needs to work, a lot better now!

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component} from "react";
+import {hot} from "react-hot-loader";
 import "./App.css";
 
-function App(){
-
+class App extends Component{
+    render(){
         return(
             <div className="App">
-            <h1> Hello, World! </h1>
+                <h1> My first React from scratch </h1>
+                <p>This has been a good exercise for me to better understand how react works as I have had to put the pieces together myself.</p>
             </div>
         );
+    }
 }
 
-export default App;
+export default hot(module)(App);
